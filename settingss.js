3a899de58aeae5228895b3634f52ddcaccd331cf
jgdvbb <growth80333@gmail.com>
2025-08-18 02:20:44 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~EzM1ERIY#5_e3GNfze994-w-jL3wLrVHcK25H1fPUUnWBpNleSCA",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "+919475846199", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
